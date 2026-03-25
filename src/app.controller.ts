b7f089a03d9cbd6b0e62007f5be122e7ee848280
Supakit 68011086 <68011086@kmitl.ac.th>
2026-03-25 11:46:04 +0700

    p01

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiResponse } from './interfaces/response.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+  let P1 : Apiresponse ={
+    "success": true,
+    "data": {
+    "service": "purchase-api",
+    "version": "1.0.0"
+  },
+  "message": console.log(getHello());
+
+
+  }
+
 }
